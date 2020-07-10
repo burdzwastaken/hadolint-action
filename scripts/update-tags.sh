cf05e565a13b69f994ee0c2b4458eee2326b7cfc
Matt Burdan <burdz@burdz.net>
2020-07-10 01:30:27 -0700

    more minor fixes

--- scripts/update-tags.sh
+++ scripts/update-tags.sh
@@ -18,4 +18,4 @@ if [[ -z "${CURRENTVERSION}" ]] || [[ -z "${NEWVERSION}" ]]; then
     die "$0 <currentversion> <newversion>"
 fi
 
-find "$ROOT" -type f -exec sed -i -e "s/${CURRENTVERSION}/${NEWVERSION}/g" {} \;
+find "$ROOT" -type f -not -iwholename '*.git*' -exec sed -i -e "s/${CURRENTVERSION}/${NEWVERSION}/g" {} \;
