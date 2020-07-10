0e0edaaea3b0438d5cbcfb28be8044ce38e6b503
Matt Burdan <burdz@burdz.net>
2020-07-10 01:26:35 -0700

    remove duplicate entry for HADOLINT_VERSION & add update-scripts.sh

---
+++ scripts/update-tags.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
+ROOT=$(cd "$(dirname "$(dirname "$0")")" && pwd)
+
+cd "$ROOT"
+
+CURRENTVERSION=${1:-}
+NEWVERSION=${2:-}
+
+if [[ -z "${CURRENTVERSION}" ]] || [[ -z "${NEWVERSION}" ]]; then
+    die "$0 <currentversion> <newversion>"
+fi
+
+find "$ROOT" -type f -exec sed -i -e "s/${CURRENTVERSION}/${NEWVERSION}/g" {} \;
